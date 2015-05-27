6fa8cb6fa3d06164295fffdcc6ebd5e80209d408
Rohit <tejal.s@indictranstech.com>
2015-05-27 10:45:06 +0530

    admin role access permission

--- digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
+++ digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
@@ -1,6 +1,6 @@
 cur_frm.cscript.refresh = function(doc){
 	cur_frm.toggle_enable("assign_qty", false);
-	if(user=='maya@digitales.com.au'){
+	if(user=='maya@digitales.com.au' || user=='Administrator'){
 		cur_frm.toggle_enable("assign_qty", true);
 	}
 }
