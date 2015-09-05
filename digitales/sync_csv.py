9c72e664c21ace16e80520249e948710f261ac06
Rohit Waghchaure <rohit.w@indictranstech.com>
2015-09-05 19:12:25 +0530

    path changes

--- digitales/sync_csv.py
+++ digitales/sync_csv.py
@@ -3,7 +3,7 @@ import csv
 
 def write_csv():
 	print "hellooo"
-	with open('/home/indictrance/Desktop/12.csv') as csvfile:
+	with open('/home/erpnext/12.csv') as csvfile:
 		reader = csv.reader(csvfile)
 		for row in reader:
 			if frappe.db.get_value("Item",row[0],"name"):
