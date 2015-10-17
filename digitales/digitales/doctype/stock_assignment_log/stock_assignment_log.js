6cc5d444da8cc4db284ef89ed09f1632f1dad15d
rohitw1991 <rohitw1991@gmail.com>
2015-10-17 19:58:24 +0530

    new ch

--- digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
+++ digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
@@ -4,3 +4,11 @@ cur_frm.cscript.refresh = function(doc){
 		cur_frm.toggle_enable("assign_qty", true);
 	}
 }
+
+cur_frm.add_fetch("sales_order", "customer_name", "customer_name")
+
+cur_frm.cscript.item_code = function(doc, cdt, cdn){
+	get_server_fields('get_item_details', '', '', doc, cdt, cdn, 1, function(){
+		refresh_field(['Media', 'item_name', 'ordered_qty'])
+	})
+}
