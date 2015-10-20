a92c187f5bc475e1c52b89db7a954354d9725b48
rohitw1991 <rohitw1991@gmail.com>
2015-10-20 11:38:16 +0530

    new ch

--- digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
+++ digitales/digitales/doctype/stock_assignment_log/stock_assignment_log.js
@@ -1,9 +1,9 @@
-cur_frm.cscript.refresh = function(doc){
-	cur_frm.toggle_enable("assign_qty", false);
-	if(user=='maya@digitales.com.au' || user=='Administrator'){
-		cur_frm.toggle_enable("assign_qty", true);
-	}
-}
+// cur_frm.cscript.refresh = function(doc){
+// 	cur_frm.toggle_enable("assign_qty", false);
+// 	if(user=='maya@digitales.com.au' || user=='Administrator'){
+// 		cur_frm.toggle_enable("assign_qty", true);
+// 	}
+// }
 
 cur_frm.add_fetch("sales_order", "customer_name", "customer_name")
 
