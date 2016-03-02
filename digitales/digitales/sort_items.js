e4c8bf10ee06b09b994200b12f0d060c367be2d3
mbauskar <mbauskar@gmail.com>
2016-03-02 15:07:17 +0530

    [fixes] freezed the screen while sorting

--- digitales/digitales/sort_items.js
+++ digitales/digitales/sort_items.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on("Delivery Note", "refresh", function(frm){
-	if(frm.doc.docstatus == 0)
+	if(frm.doc.docstatus == 0 || true)
 		cur_frm.add_custom_button(__('Sort Items'), sort_items, "icon-sort-by-alphabet", "btn-default");
 });
 
@@ -14,20 +14,24 @@ sort_items = function(){
 
 	key = items_key_mapper[cur_frm.doctype];
 	if(key){
-		items = cur_frm.doc[key];
+		frappe.dom.freeze();
+		window.setTimeout(function(){
+			items = cur_frm.doc[key];
 		
-		items.map(function(item, idx){
-			item_names.push(item.item_name);
-			item_idx[item.item_name] = idx
-		});
+			items.map(function(item, idx){
+				item_names.push(item.item_name);
+				item_idx[item.item_name] = idx;
+			});
 
-		item_names.sort()
-		$.each(item_names, function(idx, item_name){
-			index = item_idx[item_name]
-			cur_frm.doc[key][index].idx = idx+1
-		})
+			item_names.sort()
+			$.each(item_names, function(idx, item_name){
+				index = item_idx[item_name];
+				cur_frm.doc[key][index].idx = idx+1;
+			})
 
-		cur_frm.doc.__unsaved=true
-		cur_frm.refresh()
+			cur_frm.doc.__unsaved=true
+			cur_frm.refresh();
+			frappe.dom.unfreeze();
+		}, 10);
 	}
 }
