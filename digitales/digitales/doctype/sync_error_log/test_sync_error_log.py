0264911964460c5287e6361accc876fb21a1a358
mbauskar <mbauskar@gmail.com>
2016-05-03 19:31:45 +0530

    added Sync Error Log and Error Log to track sync issue

---
+++ digitales/digitales/doctype/sync_error_log/test_sync_error_log.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, digitales and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Sync Error Log')
+
+class TestSyncErrorLog(unittest.TestCase):
+	pass
